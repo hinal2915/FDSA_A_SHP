125006d18efc7fde48b92f5a0269af889585c10c
Hinal Patel <mdhinal29@gmail.com>
2026-09-21 12:38:26 +0530

    Refactor sorting algorithm in p3_2.cpp
    Replaced the Dutch National Flag algorithm with counting sort for sorting an array of 0s, 1s, and 2s.

--- Practical-3/p3_2.cpp
+++ Practical-3/p3_2.cpp
@@ -1,39 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter number of color code: ";
-    cin>>n;
+    cout << "Enter size: ";
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    int low=0;
-    int mid=0;
-    int high=n-1;
-    while(mid<=high)
+    int count0 = 0, count1 = 0, count2 = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(arr[mid]==0)
-        {
-            swap(arr[low],arr[mid]);
-            low++;
-            mid++;
-        }
-        else if(arr[mid]==1)
-        {
-            mid++;
-        }
+        if (arr[i] == 0)
+            count0++;
+        else if (arr[i] == 1)
+            count1++;
         else
-        {
-            swap(arr[mid],arr[high]);
-            high--;
-        }
+            count2++;
     }
-    for(int i=0;i<n;i++)
+    int i = 0;
+    while (count0 > 0)
     {
-        cout<<arr[i]<<" ";
+        arr[i] = 0;
+        i++;
+        count0--;
+    }
+    while (count1 > 0)
+    {
+        arr[i] = 1;
+        i++;
+        count1--;
+    }
+    while (count2 > 0)
+    {
+        arr[i] = 2;
+        i++;
+        count2--;
+    }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
